0cd4db2f8dffd1c7009c2b95d5fc5cf2af1201b8
pbridges1 <pbridges@students.mchenry.edu>
2016-09-13 22:09:25 -0500

    Add files via upload

---
+++ Madlibs2.py
@@ -0,0 +1,43 @@
+star = raw_input("Enter a noun: \n")
+# noun 1
+
+How = raw_input("Enter a Adjective: \n")
+# noun 2
+
+diamond = raw_input("Enter a thing: \n")
+# noun 3
+
+light = raw_input("Enter another noun: \n")
+# noun 4
+
+Twinkle = raw_input("Enter a verb: \n")
+# verb 1
+
+twinkle = raw_input("Enter a word that rhymes with the previous entry: \n")
+# verb 2
+
+wonder = raw_input("Enter another verb: \n")
+# verb3
+
+world = raw_input("Enter a place: \n")
+# place 1
+
+sky = raw_input("Enter an imaginary place: \n")
+# place2
+print("\n \n POEM \n")
+
+
+print Twinkle + ", " + twinkle + ", " + "little " + star
+print (How + " I " + wonder + " what you are")
+print ("Up above the " + world + " so high")
+print ("Like a " + diamond + " in the " + sky)
+print Twinkle + ", " + twinkle + ", " + "little " + star
+print (How + " I " + wonder + " what you are \n")
+
+
+print ("When the blazing sun is gone")
+print ("When he nothing shines upon")
+print ("Then you show your little " + light)
+print Twinkle + ", " + twinkle + ", " + 'all the night'
+print Twinkle + ", " + twinkle + ", " + "little " + star
+print (How + " I " + wonder + " what you are")
